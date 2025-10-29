9b870e763b2aeab2a00b5175f9b9e5df68976fd6
jojel <yopipo2024@gmail.com>
2025-10-29 18:50:06 -0500

    diseñando

--- cliente/src/componentes/navbar.js
+++ cliente/src/componentes/navbar.js
@@ -1,24 +1,27 @@
+// ...existing code...
 import React from "react";
 import '../App.css';
-const { navbarData } = require ("./navbarData");
+import { navbarData } from "./navbarData";
 
-function navbar() {
+function Navbar() {
     return (
         <div className="navbar">
             <ul className="navbarList">
                 {navbarData.map((val, key) => {
-                    return
-                    <li key={key}
-                    className="row"
-                        onClick={() => { window.location.pathname = val.link }}>
-                        {""}
-                        <div>{val.icon} </div>{""}
-                        <div>{val.title} </div>
-                    </li>
+                    return (
+                        <li
+                            key={key}
+                            className="row"
+                            onClick={() => { window.location.pathname = val.link }}
+                        >
+                            <div>{val.icon}</div>
+                            <div>{val.title}</div>
+                        </li>
+                    );
                 })}
             </ul>
         </div>
     );
 }
 
-export default navbar;
+export default Navbar;
