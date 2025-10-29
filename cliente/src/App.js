21096fe437a61f6f4602aa6165718dc1039e0be9
jojel <yopipo2024@gmail.com>
2025-10-29 17:16:15 -0500

    importé el archivo navbar al app.js

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar"; // tu componente de menú deslizable
+import IniSesion from "./pages/iniSesion";
+import Inmobiliaria from "./pages/inmobiliaria";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container mt-5">
+          <Routes>
+            <Route path="/" element={<IniSesion />} />
+            <Route path="/inmobiliaria" element={<Inmobiliaria />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
