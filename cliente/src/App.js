e5d930d95b41d05e5f1a48bbd2f6f75aaea34cfe
jojel <yopipo2024@gmail.com>
2025-10-29 17:29:25 -0500

    regreso a los mismo el app.js

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,23 +1,10 @@
 import './App.css';
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Navbar"; // tu componente de menú deslizable
-import IniSesion from "./pages/iniSesion";
-import Inmobiliaria from "./pages/inmobiliaria";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="container mt-5">
-          <Routes>
-            <Route path="/" element={<IniSesion />} />
-            <Route path="/inmobiliaria" element={<Inmobiliaria />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
+    <div className="App">
+      
+    </div>
   );
 }
 
