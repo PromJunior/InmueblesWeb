8ebd6bd5b9ebdb36a8b6f1dd1e95e4ead44e2d40
jojel <yopipo2024@gmail.com>
2025-10-29 18:40:23 -0500

    agregado de codigo

--- cliente/src/componentes/navbarData.js
+++ cliente/src/componentes/navbarData.js
@@ -1,1 +1,24 @@
-//navbar agregado de iconos para el navbar
+import React from "react";
+import HomeIcon from '@mui/icons-material/Home';
+import LoginIcon from '@mui/icons-material/Login';
+import VillaIcon from '@mui/icons-material/Villa';
+
+export const navbarData = [
+    {
+        title: 'Inicio',
+        icon: <HomeIcon/>,
+        link: '/Inicio',
+    },
+
+    {
+        title: 'IniSesion',
+        icon: <LoginIcon/>,
+        link: '/Inicio',
+    },
+
+    {
+        title: 'Inmobiliaria',
+        icon: <VillaIcon/>,
+        link: '/Inicio',
+    }
+]
